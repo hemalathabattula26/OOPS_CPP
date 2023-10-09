0ba6aac91edf237ae24a8086b7f9f4d3e23c248a
Battula B Hemalatha <119840342+hemalathabattula26@users.noreply.github.com>
2023-10-09 14:28:16 +0530

    Add files via upload

---
+++ NumberstringToSpelling.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+char words[10]={"0","1","2","3","4","5","6","7","8","9"};
+void fun(string s)
+{
+	int i,k=0
+	
+
+}
+int main()
+{
+	int n;
+	cin>>n;
+	fun(n);
+}
